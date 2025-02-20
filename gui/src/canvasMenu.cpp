477e0cebc644f39ae6d3477a1290b0987bbd7208
RooieDirk <r.dirksmits@gmail.com>
2025-02-20 17:21:48 +0100

    Correct translations of "Menu"

--- gui/src/canvasMenu.cpp
+++ gui/src/canvasMenu.cpp
@@ -287,7 +287,7 @@ void CanvasMenuHandler::MenuAppend1(wxMenu *menu, int id, wxString label) {
 #endif
 
 #ifdef __ANDROID__
-  wxFont sFont = GetOCPNGUIScaledFont(_T("Menu"));
+  wxFont sFont = GetOCPNGUIScaledFont(_("Menu"));
   item->SetFont(sFont);
 #endif
 
@@ -1110,10 +1110,10 @@ void CanvasMenuHandler::CanvasPopupMenu(int x, int y, int seltype) {
   androidEnableBackButton(false);
   androidEnableOptionsMenu(false);
 
-  setMenuStyleSheet(menuRoute, GetOCPNGUIScaledFont(_T("Menu")));
-  setMenuStyleSheet(menuWaypoint, GetOCPNGUIScaledFont(_T("Menu")));
-  setMenuStyleSheet(menuTrack, GetOCPNGUIScaledFont(_T("Menu")));
-  setMenuStyleSheet(menuAIS, GetOCPNGUIScaledFont(_T("Menu")));
+  setMenuStyleSheet(menuRoute, GetOCPNGUIScaledFont(_("Menu")));
+  setMenuStyleSheet(menuWaypoint, GetOCPNGUIScaledFont(_("Menu")));
+  setMenuStyleSheet(menuTrack, GetOCPNGUIScaledFont(_("Menu")));
+  setMenuStyleSheet(menuAIS, GetOCPNGUIScaledFont(_("Menu")));
 #endif
 
   parent->PopupMenu(menuFocus, x, y);
